96cd79e8553d7b2794de627fbe54bafcbc5a92c1
mbintangw <mbintangw@gmail.com>
2024-02-16 15:50:33 +0700

    add: MovieList

---
+++ src/Components/MovieCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const IMAGE_BASE_URL="https://image.tmdb.org/t/p/original"
+const MovieCard = ({movie}) => {
+  return (
+    <>
+      <img src={IMAGE_BASE_URL+movie.poster_path} alt="Movie Poster" 
+      className='w-28 md:w-52 rounded-lg hover:border-[3px] border-grey-400 hover:scale-110 transition-all duration-150 ease-in cursor-pointer'
+      />
+    </>
+  )
+}
+
+export default MovieCard
